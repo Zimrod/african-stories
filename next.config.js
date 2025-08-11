a46c4d2d8cdee91574b86247f475df58b50a3f71
zimrod <tumaikr@gmail.com>
2025-08-12 00:32:58 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    env: {
+      siteTitle: 'Journey of 18 Miles',
+      siteDescription: 'Engaging African Kids Tales!',
+      siteKeywords: 'african, children, books, stories, fun',
+      siteUrl: 'https://www.journey18miles.com',
+      siteImagePreviewUrl: '/icons/logo.svg',
+      twitterHandle: ''
+    },
+    images: {
+      domains: ['cdn.shopify.com'],
+    },
+  }
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import StoreHeading from '@/components/StoreHeading'
+import ProductListings from '@/components/ProductListings'
+// import { getAllProductsInCollection } from '@/lib/shopify'
+import { products } from '../data/products'
+
+function IndexPage({ products }) {
+
+  return (
+    <div className="mx-auto max-w-6xl">
+      <StoreHeading />
+      <ProductListings products={products} />      
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+//   const products = await getAllProductsInCollection()
+
+  return {
+    props: {
+      products
+    },
+  }
+}
+
+export default IndexPage
